087337b8688ed98ca6c1df774b35d6c0a6f396a5
moquan <moquanmail@163.com>
2024-02-28 16:00:42 +0800

    1. 新增 sql in 列表数据生成方法 2. 新增 jupiter 依赖

---
+++ ToolsBox/src/test/java/cn/moquan/tools/generate/GenerateSqlInListTest.java
@@ -0,0 +1,24 @@
+package cn.moquan.tools.generate;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * describe
+ * <br />
+ *
+ * @author :<b> wangYuanHong </b><br />
+ * @date :<b> 2024/2/28 14:44 </b><br />
+ */
+class GenerateSqlInListTest {
+
+    private final GenerateSqlInList generator = new GenerateSqlInList();
+
+    @Test
+    void generate() {
+
+        generator.generate();
+
+    }
+}

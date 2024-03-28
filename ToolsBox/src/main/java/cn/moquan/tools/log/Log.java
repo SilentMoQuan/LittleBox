5484e821f5767adbc47932d749bf9419f23566a4
moquan <moquanmail@163.com>
2024-03-28 17:27:39 +0800

    add log

---
+++ ToolsBox/src/main/java/cn/moquan/tools/log/Log.java
@@ -0,0 +1,39 @@
+package cn.moquan.tools.log;
+
+import org.slf4j.Logger;
+
+/**
+ * describe
+ * <br />
+ *
+ * @author :<b> wangYuanHong </b><br />
+ * @date :<b> 2024/3/28 17:20 </b><br />
+ */
+public interface Log {
+
+    String getPrefix();
+
+    Logger getLog();
+
+    default void error(String format, Object... args) {
+        String newFormat = getPrefix() + format;
+        getLog().error(newFormat, args);
+    }
+
+    default void warn(String format, Object... args) {
+        String newFormat = getPrefix() + format;
+        getLog().warn(newFormat, args);
+    }
+
+    default void info(String format, Object... args) {
+        String newFormat = getPrefix() + format;
+        getLog().info(newFormat, args);
+    }
+
+    default void debug(String format, Object... args) {
+        String newFormat = getPrefix() + format;
+        getLog().debug(newFormat, args);
+    }
+
+
+}
